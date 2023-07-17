b64c1cbd6ae637d7a941044291844e911f0388c4
Kalpesh Patel <1988.kalpesh@gmail.com>
2023-07-17 16:47:21 -0300

    Add golden test for slider pressed state.

--- test/golden_test/sliders_test.dart
+++ test/golden_test/sliders_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:tarka_ui/components/slider/slider.dart';
 
@@ -7,6 +8,7 @@ import 'flutter_test_config.dart';
 
 main() {
   _sliderNotPressed();
+  _sliderPressed();
 }
 
 void _sliderNotPressed() {
@@ -33,5 +35,58 @@ void _sliderNotPressed() {
               body: widget,
             )));
     await screenMatchesGolden(tester, "slider_not_pressed");
+  });
+}
+
+void _sliderPressed() {
+  testGoldens("Slider pressed", (tester) async {
+    final rangeSliderKey1 = GlobalKey();
+    final rangeSliderKey2 = GlobalKey();
+    var builder = StatefulBuilder(
+      builder: (context, setState) {
+        var goldenBuilder = goldenColumnBuilder()
+          ..addScenario(
+              "Slider",
+              TUISlider(
+                value: 0.1,
+                onChanged: (value) {},
+                max: 1,
+                min: 0,
+              ))
+          ..addScenario(
+              "Range Slider Left Pressed",
+              TUIRangeSlider(
+                key: rangeSliderKey1,
+                values: const TUIRangeValues(0.1, 0.9),
+                min: 0,
+                max: 1,
+                onChanged: (newValue) => {},
+              ))
+          ..addScenario(
+              "Range Slider Right Pressed",
+              TUIRangeSlider(
+                key: rangeSliderKey2,
+                values: const TUIRangeValues(0.1, 0.9),
+                min: 0,
+                max: 1,
+                onChanged: (newValue) => {},
+              ));
+        return goldenBuilder.build();
+      },
+    );
+    await tester.pumpWidgetBuilder(builder,
+        wrapper: (widget) => tuiAppWrapper()(Scaffold(
+              body: widget,
+            )));
+    var sliderRect = tester.getRect(find.byType(TUISlider));
+    final rangeSliderRect1 = tester.getRect(find.byKey(rangeSliderKey1));
+    final rangeSliderRect2 = tester.getRect(find.byKey(rangeSliderKey2));
+    await tester.startGesture(
+        sliderRect.centerLeft.translate(sliderRect.width * 0.1, 0));
+    await tester.startGesture(
+        rangeSliderRect1.centerLeft.translate(rangeSliderRect1.width * 0.1, 0));
+    await tester.startGesture(
+        rangeSliderRect2.centerRight.translate(sliderRect.width * -0.1, 0));
+    await screenMatchesGolden(tester, "slider_pressed");
   });
 }
