495fcde23c7fb40a516bc58e58a57d5494ae3e44
Kalpesh Patel <1988.kalpesh@gmail.com>
2023-07-18 10:04:36 -0300

    Add test for TUIRangeSlider callbacks.

--- test/widget_test/slider_test.dart
+++ test/widget_test/slider_test.dart
@@ -13,6 +13,12 @@ main() {
     _testSliderOnChangeStartCallback();
     _testSliderOnChangeEndCallback();
   });
+
+  group("TUIRangeSlider callbacks", () {
+    _testRangeSliderOnChangedCallback();
+    _testRangeSliderOnChangeStartCallback();
+    _testRangeSliderOnChangeEndCallback();
+  });
 }
 
 void _testSliderOnChangedCallback() {
@@ -92,5 +98,88 @@ void _testSliderOnChangeEndCallback() {
     expect(completer.isCompleted, true);
     //Should receive new/changed value.
     expect(await completer.future, 0.5);
+  });
+}
+
+void _testRangeSliderOnChangedCallback() {
+  testWidgets("should receive latest/new value in onChanged callback",
+      (widgetTester) async {
+    var sliderValue = const TUIRangeValues(0.1, 0.9);
+    await widgetTester.pumpWidgetBuilder(Scaffold(
+      body: StatefulBuilder(
+        builder: (context, setState) {
+          return TUIRangeSlider(
+            values: sliderValue,
+            min: 0,
+            max: 1,
+            onChanged: (value) {
+              setState(() => sliderValue = value);
+            },
+          );
+        },
+      ),
+    ), wrapper: tuiAppWrapper());
+    final rect = widgetTester.getRect(find.byType(TUIRangeSlider));
+    await widgetTester.dragFrom(rect.centerLeft.translate(rect.width * 0.1, 0),
+        Offset.fromDirection(0, rect.width * 0.4));
+    expect(sliderValue, const TUIRangeValues(0.5, 0.9));
+  });
+}
+
+void _testRangeSliderOnChangeStartCallback() {
+  testWidgets("should receive initial/stating value in onChangeStart callback",
+      (widgetTester) async {
+    var sliderValue = const TUIRangeValues(0.1, 0.9);
+    var completer = Completer<TUIRangeValues>();
+    await widgetTester.pumpWidgetBuilder(Scaffold(
+      body: StatefulBuilder(
+        builder: (context, setState) {
+          return TUIRangeSlider(
+            values: sliderValue,
+            min: 0,
+            max: 1,
+            onChanged: (value) {
+              setState(() => sliderValue = value);
+            },
+            onChangeStart: completer.complete,
+          );
+        },
+      ),
+    ), wrapper: tuiAppWrapper());
+    final rect = widgetTester.getRect(find.byType(TUIRangeSlider));
+    await widgetTester.dragFrom(rect.centerLeft.translate(rect.width * 0.1, 0),
+        Offset.fromDirection(0, rect.width * 0.4));
+    expect(completer.isCompleted, true);
+//Should receive old/starting value.
+    expect(await completer.future, const TUIRangeValues(0.1, 0.9));
+  });
+}
+
+void _testRangeSliderOnChangeEndCallback() {
+  testWidgets("should receive end/latest value in onChangeEnd callback",
+      (widgetTester) async {
+    var sliderValue = const TUIRangeValues(0.1, 0.9);
+    var completer = Completer<TUIRangeValues>();
+    await widgetTester.pumpWidgetBuilder(Scaffold(
+      body: StatefulBuilder(
+        builder: (context, setState) {
+          return TUIRangeSlider(
+            values: sliderValue,
+            min: 0,
+            max: 1,
+            onChanged: (value) {
+              setState(() => sliderValue = value);
+            },
+            onChangeEnd: completer.complete,
+          );
+        },
+      ),
+    ), wrapper: tuiAppWrapper());
+    final rect = widgetTester.getRect(find.byType(TUIRangeSlider));
+    await widgetTester.dragFrom(rect.centerLeft.translate(rect.width * 0.1, 0),
+        Offset.fromDirection(0, rect.width * 0.4));
+    expect(completer.isCompleted, true);
+//Should receive new/changed value.
+    expect(await completer.future, const TUIRangeValues(0.5, 0.9));
   });
 }
