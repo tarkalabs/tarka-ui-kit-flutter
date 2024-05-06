bba0b697eb6f2ba6b1528f23b1e6c9ed8274394c
Kalpesh Patel <1988.kalpesh@gmail.com>
2024-05-06 14:11:34 +0530

    Color token changes for V2 (#74)
    * Update packages
    
    * Update Color style.
    
    * Use TUI color scheme in material color scheme for better coupling
    
    * Update Color values for light theme and remove unused colors.
    
    * Use new color scheme in example application.
    
    * Fix colors in test.
    
    * Fix tests.
    
    * Keeping consistency with rest of the color declaration.

--- test/widget_test/icon_button_test.dart
+++ test/widget_test/icon_button_test.dart
@@ -1,7 +1,6 @@
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tarka_ui/components/button/style.dart';
 import 'package:tarka_ui/tarka_ui.dart';
 
 import '../defaults.dart';
